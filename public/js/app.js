c53660fc774fc17c1e02dc48689cf84b74d164f8
Paulo jorge xavier dos santos <152025930+Pjxsantos@users.noreply.github.com>
2024-01-25 22:26:18 -0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 200,
+        "value": 120,
         "density": {
           "enable": true,
           "value_area": 800
